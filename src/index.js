9f22767ed89fa440f74a144b66b7913958f2cbda
UncleJerry <jared.myhrberg@gmail.com>
2019-03-15 17:31:22 -0700

    simple fetch

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// open local host and you should be able to play with this
+
+fetch('https://gateway.marvel.com:443/v1/public/characters?name=storm&apikey=698ecfea67de32ae8e6a3b78e74af2b3')
+    .then(response => response.json())
+    .then(results => {
+        console.log(results);
+    });
