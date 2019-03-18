7b223466197dc7daf8f93dab9145331c6f8ae754
UncleJerry <jared.myhrberg@gmail.com>
2019-03-18 13:35:07 -0700

    function to dynamically generate header/profile

---
+++ src/shared/header.js
@@ -0,0 +1,41 @@
+export function makeHeaderTemplate() {
+    const html = `
+    <header>
+        <a href="index.html"><img src="./assets/marvel-us-logo-color.svg" alt="MarvelUS"></a>
+    </header>
+    `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+export function makeProfileTemplate(user) {
+    let html = null;
+    if(!user) {
+        html = /*html*/`
+        <nav>
+            <a href="/auth.html" id="login">Log In/Sign Up</a>
+        </nav>
+        `;
+    } else {
+        html = /*html*/`
+        <nav>
+            <div id="profile-header">
+                <ul>
+                    <li><a href="./wishlist.html">Wishlist</a></li>
+                    <li><a href="./library.html">Library</a></li>
+                    <li><span id="user-name-display">${user.displayName}</span></li>
+                    <ul id="drop-down">
+                        <li><a href="./user-profile.html">User Profile</a></li>
+                        <li><span id="log-out">Log Out</span></li>
+                    </ul>
+                </ul>
+            </div>
+        </nav>
+        `;
+    }
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
--- test/functions/header.test.js
+++ test/functions/header.test.js
@@ -1,17 +1,7 @@
+import { makeHeaderTemplate, makeProfileTemplate } from '../../src/shared/header.js';
 const test = QUnit.test;
 
 QUnit.module('header');
-
-function makeHeaderTemplate() {
-    const html = `
-    <header>
-        <a href="index.html"><img src="./assets/marvel-us-logo-color.svg" alt="MarvelUS"></a>
-    </header>
-    `;
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
 
 test('static header html?', assert => {
     // arrange
@@ -26,18 +16,6 @@ test('static header html?', assert => {
     assert.htmlEqual(results, expected);
 });
 
-
-function makeProfileTemplate() {
-    const html = /*html*/`
-    <nav>
-        <a href="/auth.html" id="login">Log In/Sign Up</a>
-    </nav>
-    `;
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content;
-}
-
 test('make profile html without user', assert => {
     // arrange
     const expected = `
@@ -51,16 +29,30 @@ test('make profile html without user', assert => {
     assert.htmlEqual(results, expected);
 });
 
+
 test('make profile html with user', assert => {
+    const user = {
+        displayName: 'Tom Hanks'
+    };
+
     // arrange
     const expected = `
     <nav>
-        <a href
-        <span id="logout">Log Out</span>
+        <div id="profile-header">
+            <ul>
+                <li><a href="./wishlist.html">Wishlist</a></li>
+                <li><a href="./library.html">Library</a></li>
+                <li><span id="user-name-display">Tom Hanks</span></li>
+                <ul id="drop-down">
+                    <li><a href="./user-profile.html">User Profile</a></li>
+                    <li><span id="log-out">Log Out</span></li>
+                </ul>
+            </ul>
+        </div>
     </nav>
     `;
     // act
-    const results = makeProfileTemplate();
+    const results = makeProfileTemplate(user);
     // assert
     assert.htmlEqual(results, expected);
 });
