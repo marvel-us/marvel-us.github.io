3d56f638629a43026287d1c9b3e7c7c71b7ee960
UncleJerry <jared.myhrberg@gmail.com>
2019-03-18 12:06:23 -0700

    taking lunch

--- test/functions/header.test.js
+++ test/functions/header.test.js
@@ -27,13 +27,40 @@ test('static header html?', assert => {
 });
 
 
-
+function makeProfileTemplate() {
+    const html = /*html*/`
+    <nav>
+        <a href="/auth.html" id="login">Log In/Sign Up</a>
+    </nav>
+    `;
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
 
 test('make profile html without user', assert => {
     // arrange
-
+    const expected = `
+    <nav>
+        <a href="/auth.html" id="login">Log In/Sign Up</a>
+    </nav>
+    `;
     // act
-
+    const results = makeProfileTemplate();
     // assert
-    assert.htmlEqual
+    assert.htmlEqual(results, expected);
+});
+
+test('make profile html with user', assert => {
+    // arrange
+    const expected = `
+    <nav>
+        <a href
+        <span id="logout">Log Out</span>
+    </nav>
+    `;
+    // act
+    const results = makeProfileTemplate();
+    // assert
+    assert.htmlEqual(results, expected);
 });
