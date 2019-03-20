45fbb121256e7bc0f1b27910bc16c93bbfc295ed
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-20 15:15:09 -0700

    wishlist.js/wishlist loading

---
+++ src/profile/wishlist.js
@@ -0,0 +1,16 @@
+import { auth, wishlistByUserRef } from '../firebase/firebase.js';
+import loadComics from '../comics/comic-list-components.js';
+import loadHeader from '../shared/header.js';
+import objectToArray from '../object-to-array.js';
+
+loadHeader();
+
+auth.onAuthStateChanged(user => {
+    const userId = user.uid;
+    const userWishlistRef = wishlistByUserRef.child(userId);
+    userWishlistRef.on('value', snapshot => {
+        const value = snapshot.val();
+        const comics = objectToArray(value);
+        loadComics(comics);
+    });
+});
